a18c557a504721c00b247a0c9bda5acf8bb0fce5
Adrian Ulrich <adrian@blinkenlights.ch>
2019-08-05 19:00:19 +0200

    Use os.File for raw recv. socket in nonblocking mode as this works better than our homebrewed version.

--- lib/rsocks/recv.go
+++ lib/rsocks/recv.go
@@ -2,6 +2,7 @@ package rsocks
 
 import (
 	"net"
+	"os"
 	"syscall"
 )
 
@@ -9,12 +10,11 @@ type rrsock struct {
 	fd int
 }
 
-func GetRawRecvSock(iface *net.Interface) (*rrsock, error) {
+func GetRawRecvSock(iface *net.Interface) (*os.File, error) {
 	s, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_DGRAM, int(htons(syscall.ETH_P_IP)))
 	if err != nil {
 		return nil, err
 	}
-
 	sll := &syscall.SockaddrLinklayer{
 		Protocol: htons(syscall.ETH_P_IP),
 		Ifindex:  iface.Index,
@@ -23,13 +23,9 @@ func GetRawRecvSock(iface *net.Interface) (*rrsock, error) {
 		syscall.Close(s)
 		return nil, err
 	}
-	return &rrsock{fd: s}, nil
-}
-
-func (rr *rrsock) Read(b []byte) (int, error) {
-	return syscall.Read(rr.fd, b)
-}
-
-func (rr *rrsock) Close() error {
-	return syscall.Close(rr.fd)
+	if err := syscall.SetNonblock(s, true); err != nil {
+		syscall.Close(s)
+		return nil, err
+	}
+	return os.NewFile(uintptr(s), ""), nil
 }
