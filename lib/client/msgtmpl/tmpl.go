a1fddd3c759132e05d72c67ac594b40961c18ab9
Adrian Ulrich <adrian@blinkenlights.ch>
2019-08-15 21:15:29 +0200

    cleanup msg template

--- lib/client/msgtmpl/tmpl.go
+++ lib/client/msgtmpl/tmpl.go
@@ -32,21 +32,30 @@ func New(iface *net.Interface, xid uint32) tmpl {
 
 // Discover returns a DHCPDISCOVER message.
 func (rx *tmpl) Discover() []byte {
+	// This message is broadcasted in an unconfigured state.
+	// We do not yet know our own IP.
 	return rx.request(dhcpmsg.MsgTypeDiscover, ipNone, ipBcast, nil, nil)
 }
 
 // RequestSelecting returns a new selecting request.
 func (rx *tmpl) RequestSelecting(requestedIP, serverIdentifier net.IP) []byte {
-	return rx.request(dhcpmsg.MsgTypeRequest, net.IPv4(0, 0, 0, 0), net.IPv4(255, 255, 255, 255),
-		&requestedIP, &serverIdentifier)
+	// This message is broadcasted after we received an IP offer.
+	// The client is still unconfigured but picked a server and has an IP it attempts to request.
+	return rx.request(dhcpmsg.MsgTypeRequest, ipNone, ipBcast, &requestedIP, &serverIdentifier)
 }
 
 // RequestRenewing returns a renewing request.
 func (rx *tmpl) RequestRenewing(requestedIP, serverIdentifier net.IP) []byte {
+	// This is an unicast message of a configured client.
+	// We only supply a source (our) and destination (old server identifier) IP.
+	// The ServerIdentifier and RequestedIP options must not be set in this state.
 	return rx.request(dhcpmsg.MsgTypeRequest, requestedIP, serverIdentifier, nil, nil)
 }
 
 // RequestRebinding returns a rebinding request.
-func (rx *tmpl) RequestRebinding(requestedIP, serverIdentifier net.IP) []byte {
-	return rx.request(dhcpmsg.MsgTypeRequest, requestedIP, serverIdentifier, nil, nil)
+func (rx *tmpl) RequestRebinding(requestedIP net.IP) []byte {
+	// This is a broadcast message of a configured client.
+	// This message is similar to the RequestRenewing message bug sent as
+	// a broadcast message to all (DHCP)Servers on the network.
+	return rx.request(dhcpmsg.MsgTypeRequest, requestedIP, ipBcast, nil, nil)
 }
---
+++ lib/client/msgtmpl/tmpl_test.go
@@ -0,0 +1,245 @@
+package msgtmpl
+
+import (
+	"net"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"gitlab.com/adrian_blx/psa-dhcp/lib/dhcpmsg"
+	"gitlab.com/adrian_blx/psa-dhcp/lib/layer"
+)
+
+var (
+	testXid        = uint32(9)
+	testSource     = net.IPv4(1, 2, 3, 9)
+	testIdentifier = net.IPv4(21, 23, 24, 25)
+	testIface      = net.Interface{HardwareAddr: []byte{1, 2, 3, 4, 5, 6}}
+	testMAC        = [6]byte{1, 2, 3, 4, 5, 6}
+)
+
+type bundle struct {
+	IPv4 layer.IPv4
+	UDP  layer.UDP
+	Msg  dhcpmsg.Message
+}
+
+func TestDiscover(t *testing.T) {
+	want := bundle{
+		IPv4: layer.IPv4{
+			TTL:      0xfa,
+			Protocol: 0x11,
+			// we are not configured yet.
+			Source: ipNone,
+			// don't know any servers.
+			Destination: ipBcast,
+		},
+		UDP: layer.UDP{
+			SrcPort: 68,
+			DstPort: 67,
+		},
+		// ServerIdentifier MUST
+		// ClientIP must not
+		// Requested IP MUST
+		Msg: dhcpmsg.Message{
+			Xid:       testXid,
+			Op:        dhcpmsg.OpRequest,
+			Htype:     dhcpmsg.HtypeETHER,
+			Cookie:    dhcpmsg.DHCPCookie,
+			Hlen:      6,
+			ClientIP:  ipNone,
+			ClientMAC: testMAC,
+			YourIP:    ipNone,
+			RelayIP:   ipNone,
+			NextIP:    ipNone,
+			Options: []dhcpmsg.DHCPOpt{
+				dhcpmsg.OptionType(dhcpmsg.MsgTypeDiscover),
+				dhcpmsg.OptionClientIdentifier(testMAC),
+				dhcpmsg.OptionMaxMessageSize(maxMsgSize),
+				dhcpmsg.OptionHostname("TEST"),
+			},
+		},
+	}
+
+	tmpl := New(&testIface, testXid)
+	got, err := undo(tmpl.Discover())
+	if err != nil {
+		t.Errorf("TestRequestSelecting = %v, want nil err", err)
+	}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("TestRequestSelecting had diff: %s", diff)
+	}
+}
+
+func TestRequestSelecing(t *testing.T) {
+	want := bundle{
+		IPv4: layer.IPv4{
+			TTL:      0xfa,
+			Protocol: 0x11,
+			// we are not configured yet.
+			Source: ipNone,
+			// selected a server, but must broadcast.
+			Destination: ipBcast,
+		},
+		UDP: layer.UDP{
+			SrcPort: 68,
+			DstPort: 67,
+		},
+		// ServerIdentifier MUST
+		// ClientIP must not
+		// Requested IP MUST
+		Msg: dhcpmsg.Message{
+			Xid:       testXid,
+			Op:        dhcpmsg.OpRequest,
+			Htype:     dhcpmsg.HtypeETHER,
+			Cookie:    dhcpmsg.DHCPCookie,
+			Hlen:      6,
+			ClientIP:  ipNone,
+			ClientMAC: testMAC,
+			YourIP:    ipNone,
+			RelayIP:   ipNone,
+			NextIP:    ipNone,
+			Options: []dhcpmsg.DHCPOpt{
+				dhcpmsg.OptionType(dhcpmsg.MsgTypeRequest),
+				dhcpmsg.OptionClientIdentifier(testMAC),
+				dhcpmsg.OptionMaxMessageSize(maxMsgSize),
+				dhcpmsg.OptionRequestedIP(testSource),
+				dhcpmsg.OptionServerIdentifier(testIdentifier),
+				dhcpmsg.OptionHostname("TEST"),
+			},
+		},
+	}
+
+	tmpl := New(&testIface, testXid)
+	got, err := undo(tmpl.RequestSelecting(testSource, testIdentifier))
+	if err != nil {
+		t.Errorf("TestRequestSelecting = %v, want nil err", err)
+	}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("TestRequestSelecting had diff: %s", diff)
+	}
+}
+
+func TestRequestRenewing(t *testing.T) {
+	want := bundle{
+		IPv4: layer.IPv4{
+			TTL:      0xfa,
+			Protocol: 0x11,
+			Source:   testSource,
+			// This is NOT a broadcast message.
+			Destination: testIdentifier,
+		},
+		UDP: layer.UDP{
+			SrcPort: 68,
+			DstPort: 67,
+		},
+		// ServerIdentifier MUST NOT
+		// requested IP MUST NOT
+		// ClientIP MUST
+		Msg: dhcpmsg.Message{
+			Xid:       testXid,
+			Op:        dhcpmsg.OpRequest,
+			Htype:     dhcpmsg.HtypeETHER,
+			Cookie:    dhcpmsg.DHCPCookie,
+			Hlen:      6,
+			ClientIP:  testSource,
+			ClientMAC: testMAC,
+			YourIP:    ipNone,
+			RelayIP:   ipNone,
+			NextIP:    ipNone,
+			Options: []dhcpmsg.DHCPOpt{
+				dhcpmsg.OptionType(dhcpmsg.MsgTypeRequest),
+				dhcpmsg.OptionClientIdentifier(testMAC),
+				dhcpmsg.OptionMaxMessageSize(maxMsgSize),
+				dhcpmsg.OptionHostname("TEST"),
+			},
+		},
+	}
+
+	tmpl := New(&testIface, testXid)
+	got, err := undo(tmpl.RequestRenewing(testSource, testIdentifier))
+	if err != nil {
+		t.Errorf("TestRequestRenewing = %v, want nil err", err)
+	}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("TestRequestRenewing had diff: %s", diff)
+	}
+}
+
+func TestRequestRebinding(t *testing.T) {
+	want := bundle{
+		IPv4: layer.IPv4{
+			TTL:         0xfa,
+			Protocol:    0x11,
+			Source:      testSource,
+			Destination: ipBcast,
+		},
+		UDP: layer.UDP{
+			SrcPort: 68,
+			DstPort: 67,
+		},
+		// ServerIdentifier MUST NOT
+		// requested IP MUST NOT
+		// ClientIP MUST
+		Msg: dhcpmsg.Message{
+			Xid:       testXid,
+			Op:        dhcpmsg.OpRequest,
+			Htype:     dhcpmsg.HtypeETHER,
+			Cookie:    dhcpmsg.DHCPCookie,
+			Hlen:      6,
+			ClientIP:  testSource,
+			ClientMAC: testMAC,
+			YourIP:    ipNone,
+			RelayIP:   ipNone,
+			NextIP:    ipNone,
+			Options: []dhcpmsg.DHCPOpt{
+				dhcpmsg.OptionType(dhcpmsg.MsgTypeRequest),
+				dhcpmsg.OptionClientIdentifier(testMAC),
+				dhcpmsg.OptionMaxMessageSize(maxMsgSize),
+				dhcpmsg.OptionHostname("TEST"),
+			},
+		},
+	}
+
+	tmpl := New(&testIface, testXid)
+	got, err := undo(tmpl.RequestRebinding(testSource))
+	if err != nil {
+		t.Errorf("TestRequestBinding = %v, want nil err", err)
+	}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("TestRequestBinding had diff: %s", diff)
+	}
+}
+
+func undo(raw []byte) (bundle, error) {
+	v4, err := layer.DecodeIPv4(raw)
+	if err != nil {
+		return bundle{}, err
+	}
+
+	udp, err := layer.DecodeUDP(v4.Data)
+	if err != nil {
+		return bundle{}, err
+	}
+
+	msg, err := dhcpmsg.Decode(udp.Data)
+	if err != nil {
+		return bundle{}, err
+	}
+
+	v4.Identification = 0
+	v4.Checksum = 0
+	v4.Data = nil
+	udp.Data = nil
+
+	// Remove hostname to have this stable.
+	var opts []dhcpmsg.DHCPOpt
+	for _, o := range msg.Options {
+		if o.Option != dhcpmsg.OptHostname {
+			opts = append(opts, o)
+		}
+	}
+	opts = append(opts, dhcpmsg.OptionHostname("TEST"))
+	msg.Options = opts
+
+	return bundle{IPv4: *v4, UDP: *udp, Msg: *msg}, nil
+}
