be8a5b813cda29ec56e78e80b78263a67355bc95
Adrian Ulrich <adrian@blinkenlights.ch>
2019-08-18 12:19:26 +0200

    add ifmon library

---
+++ lib/ifmon/netlink.go
@@ -0,0 +1,36 @@
+package ifmon
+
+import (
+	"context"
+	"net"
+	"syscall"
+
+	"github.com/vishvananda/netlink"
+)
+
+// MonitorChanges checks for when the given interface came 'back up'.
+func MonitorChanges(ctx context.Context, iface *net.Interface, event chan<- bool) error {
+	update := make(chan netlink.LinkUpdate, 64)
+	done := make(chan struct{})
+
+	if err := netlink.LinkSubscribe(update, done); err != nil {
+		return err
+	}
+	defer close(done)
+
+	oldState := uint32(syscall.IFF_RUNNING)
+	for {
+		select {
+		case msg := <-update:
+			flags := msg.IfInfomsg.Flags & syscall.IFF_RUNNING
+			if flags != oldState && iface.Index == int(msg.IfInfomsg.Index) {
+				oldState = flags
+				if flags&syscall.IFF_RUNNING != 0 {
+					event <- true
+				}
+			}
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
