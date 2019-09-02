6dfff9c47a720c9d0e8e98317ac4f54625e6ff34
Adrian Ulrich <adrian@blinkenlights.ch>
2019-09-02 21:46:42 +0200

    return nicer duid string

--- lib/server/ipdb/duid/duid.go
+++ lib/server/ipdb/duid/duid.go
@@ -7,5 +7,16 @@ import (
 type Duid []byte
 
 func (d Duid) String() string {
-	return fmt.Sprintf("duid(%x)", []byte(d))
+	if len(d) == 0 {
+		return "<duid:nil>"
+	}
+	buf := make([]byte, 0, len(d)*3-1)
+	for i, b := range d {
+		if i > 0 {
+			buf = append(buf, '-')
+		}
+		buf = append(buf, []byte(fmt.Sprintf("%02x", b))...)
+	}
+
+	return fmt.Sprintf("<duid:%s>", string(buf))
 }
