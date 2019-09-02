5359c24552857a4a1d820d3a22b11c53fea2f803
Adrian Ulrich <adrian@blinkenlights.ch>
2019-09-02 19:44:37 +0200

    add more tests

--- lib/client/verify/verifyer_test.go
+++ lib/client/verify/verifyer_test.go
@@ -8,6 +8,183 @@ import (
 	"gitlab.com/adrian_blx/psa-dhcp/lib/dhcpmsg"
 )
 
+func TestVerifyGenAck(t *testing.T) {
+	input := []struct {
+		name          string
+		xid           uint32
+		msg           dhcpmsg.Message
+		opts          dhcpmsg.DecodedOptions
+		lastmsg       dhcpmsg.Message
+		lastopts      dhcpmsg.DecodedOptions
+		wantSelecting State
+		wantRenewing  State
+		wantRebinding State
+	}{
+		{
+			name:          "empty struct",
+			wantSelecting: Failed,
+			wantRenewing:  Failed,
+			wantRebinding: Failed,
+		},
+		{
+			name: "okay",
+			xid:  33,
+			lastmsg: dhcpmsg.Message{
+				YourIP: net.IPv4(192, 168, 1, 1),
+			},
+			lastopts: dhcpmsg.DecodedOptions{
+				ServerIdentifier: net.IPv4(192, 168, 9, 99),
+			},
+			msg: dhcpmsg.Message{
+				Xid:    33,
+				YourIP: net.IPv4(192, 168, 1, 1),
+			},
+			opts: dhcpmsg.DecodedOptions{
+				ServerIdentifier:       net.IPv4(192, 168, 9, 99),
+				Routers:                []net.IP{net.IPv4(192, 168, 0, 1)},
+				MessageType:            dhcpmsg.MsgTypeAck,
+				IPAddressLeaseDuration: 1 * time.Minute,
+			},
+			wantSelecting: Passed,
+			wantRenewing:  Passed,
+			wantRebinding: Passed,
+		},
+		{
+			name: "bad xid",
+			xid:  66,
+			lastmsg: dhcpmsg.Message{
+				YourIP: net.IPv4(192, 168, 1, 1),
+			},
+			lastopts: dhcpmsg.DecodedOptions{
+				ServerIdentifier: net.IPv4(192, 168, 9, 99),
+			},
+			msg: dhcpmsg.Message{
+				Xid:    33,
+				YourIP: net.IPv4(192, 168, 1, 1),
+			},
+			opts: dhcpmsg.DecodedOptions{
+				ServerIdentifier:       net.IPv4(192, 168, 9, 99),
+				Routers:                []net.IP{net.IPv4(192, 168, 0, 1)},
+				MessageType:            dhcpmsg.MsgTypeAck,
+				IPAddressLeaseDuration: 1 * time.Minute,
+			},
+			wantSelecting: Failed,
+			wantRenewing:  Failed,
+			wantRebinding: Failed,
+		},
+		{
+			name: "diverged server identifier",
+			xid:  33,
+			lastmsg: dhcpmsg.Message{
+				YourIP: net.IPv4(192, 168, 1, 1),
+			},
+			lastopts: dhcpmsg.DecodedOptions{},
+			msg: dhcpmsg.Message{
+				Xid:    33,
+				YourIP: net.IPv4(192, 168, 1, 1),
+			},
+			opts: dhcpmsg.DecodedOptions{
+				ServerIdentifier:       net.IPv4(192, 168, 9, 99),
+				Routers:                []net.IP{net.IPv4(192, 168, 0, 1)},
+				MessageType:            dhcpmsg.MsgTypeAck,
+				IPAddressLeaseDuration: 1 * time.Minute,
+			},
+			wantSelecting: Failed,
+			wantRenewing:  Failed,
+			wantRebinding: Failed,
+		},
+		{
+			name: "diverged IP",
+			xid:  33,
+			lastmsg: dhcpmsg.Message{
+				YourIP: net.IPv4(192, 168, 9, 1),
+			},
+			lastopts: dhcpmsg.DecodedOptions{
+				ServerIdentifier: net.IPv4(192, 168, 9, 99),
+			},
+			msg: dhcpmsg.Message{
+				Xid:    33,
+				YourIP: net.IPv4(192, 168, 1, 1),
+			},
+			opts: dhcpmsg.DecodedOptions{
+				ServerIdentifier:       net.IPv4(192, 168, 9, 99),
+				Routers:                []net.IP{net.IPv4(192, 168, 0, 1)},
+				MessageType:            dhcpmsg.MsgTypeAck,
+				IPAddressLeaseDuration: 1 * time.Minute,
+			},
+			wantSelecting: Failed,
+			wantRenewing:  Failed,
+			wantRebinding: Failed,
+		},
+		{
+			name: "bad msg type",
+			xid:  33,
+			lastmsg: dhcpmsg.Message{
+				YourIP: net.IPv4(192, 168, 1, 1),
+			},
+			lastopts: dhcpmsg.DecodedOptions{
+				ServerIdentifier: net.IPv4(192, 168, 9, 99),
+			},
+			msg: dhcpmsg.Message{
+				Xid:    33,
+				YourIP: net.IPv4(192, 168, 1, 1),
+			},
+			opts: dhcpmsg.DecodedOptions{
+				ServerIdentifier:       net.IPv4(192, 168, 9, 99),
+				Routers:                []net.IP{net.IPv4(192, 168, 0, 1)},
+				MessageType:            dhcpmsg.MsgTypeOffer,
+				IPAddressLeaseDuration: 1 * time.Minute,
+			},
+			wantSelecting: Failed,
+			wantRenewing:  Failed,
+			wantRebinding: Failed,
+		},
+		{
+			name:    "nack",
+			xid:     33,
+			lastmsg: dhcpmsg.Message{},
+			lastopts: dhcpmsg.DecodedOptions{
+				ServerIdentifier: net.IPv4(192, 168, 9, 99),
+			},
+			msg: dhcpmsg.Message{},
+			opts: dhcpmsg.DecodedOptions{
+				ServerIdentifier: net.IPv4(192, 168, 9, 99),
+				MessageType:      dhcpmsg.MsgTypeNack,
+			},
+			wantSelecting: IsNack,
+			wantRenewing:  IsNack,
+			wantRebinding: IsNack,
+		},
+		{
+			name:    "alien nack",
+			xid:     33,
+			lastmsg: dhcpmsg.Message{},
+			lastopts: dhcpmsg.DecodedOptions{
+				ServerIdentifier: net.IPv4(192, 168, 9, 99),
+			},
+			msg: dhcpmsg.Message{},
+			opts: dhcpmsg.DecodedOptions{
+				ServerIdentifier: net.IPv4(192, 168, 100, 99),
+				MessageType:      dhcpmsg.MsgTypeNack,
+			},
+			wantSelecting: Failed,
+			wantRenewing:  Failed,
+			wantRebinding: Failed,
+		},
+	}
+
+	for _, test := range input {
+		if got := VerifySelectingAck(test.lastmsg, test.lastopts, test.xid)(test.msg, test.opts); got != test.wantSelecting {
+			t.Errorf("VerifySelectingAck(%s) = %d, want %d", test.name, got, test.wantSelecting)
+		}
+		if got := VerifyRenewingAck(test.lastmsg, test.lastopts, test.xid)(test.msg, test.opts); got != test.wantRenewing {
+			t.Errorf("VerifyRenewingAck(%s) = %d, want %d", test.name, got, test.wantRenewing)
+		}
+		if got := VerifyRebindingAck(test.lastmsg, test.lastopts, test.xid)(test.msg, test.opts); got != test.wantRebinding {
+			t.Errorf("VerifyRebindingAck(%s) = %d, want %d", test.name, got, test.wantRebinding)
+		}
+	}
+}
 func TestVerifyOffer(t *testing.T) {
 	input := []struct {
 		name string
@@ -125,8 +302,7 @@ func TestVerifyOffer(t *testing.T) {
 	}
 
 	for _, test := range input {
-		got := VerifyOffer(test.xid)(test.msg, test.opts)
-		if got != test.want {
+		if got := VerifyOffer(test.xid)(test.msg, test.opts); got != test.want {
 			t.Errorf("VerifyOffer(%s) = %d, want %d", test.name, got, test.want)
 		}
 	}
